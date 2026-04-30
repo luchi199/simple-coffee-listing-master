615a8c60b8ed9d6a1bdba86ac871049588900492
luchi199 <umorenidy660@gmail.com>
2026-04-30 01:37:47 -0600

    Completed Loader UI state

--- simple-coffee-listing-master/src/components/Products.jsx
+++ simple-coffee-listing-master/src/components/Products.jsx
@@ -1,18 +1,22 @@
 import { useProducts } from "../hooks/useProducts";
 import ProductCard from "./ProductCard";
+import Loader from "./ui/Loader";
+import Error from "./ui/Error";
 
 export default function Products() {
   const { products, isLoading, error } = useProducts();
 
   return (
-    <div className="bg-gray-800">
-      {isLoading
-        ? "loader goes here"
-        : error
-          ? "error state goes here"
-          : products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+    <div className="flex flex-wrap bg-gray-800 w-full">
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Error />
+      ) : (
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))
+      )}
     </div>
   );
 }
---
+++ simple-coffee-listing-master/src/components/ui/Loader.jsx
@@ -0,0 +1,5 @@
+export default function Loader() {
+  return (
+    <div className="border-8 border-blue-100 border-r-amber-300 w-12 h-12 aspect-square rounded-full animate-spin text-black"></div>
+  );
+}
